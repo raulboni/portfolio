9244f8e390c74f756866dbfaeea8d606cf428a0a
Raúl Bonilla <raulbonilla@outlook.com>
2022-08-03 15:01:16 +0200

    changes to projects info

--- public/projects/projectsData.js
+++ public/projects/projectsData.js
@@ -1,7 +1,8 @@
 export const projects = [
   {
-    title: "Grupos Random",
-    description: "App that allows teachers to make random groups of students.",
+    title: "Random Team Generator",
+    description:
+      "Web application that uses authentication. User can store lists of names which can be edited and can generate random groups.",
     img: "/projects/grupos-random.png",
     techs: ["React.js", "CSS Modules", "Firestore", "Firebase Authentication"],
     link: "https://group-randomizer-59197.web.app/",
@@ -11,10 +12,19 @@ export const projects = [
   {
     title: "Rick and Morty Characters",
     description:
-      "This app allows user to filter Rick and Morty characters and displays some info.",
+      "Web application that shows Rick and Morty characters. User can apply filters, search by text and click on the character to get some extra info.",
     img: "/projects/rick-morty-characters.png",
-    techs: ["React.js", "Next.js", "API-REST", "Tailwind-CSS"],
-    link: "https://group-randomizer-59197.web.app/",
-    github: "https://github.com/raulboni/random-groups",
+    techs: ["React.js", "Next.js", "API REST", "Tailwind CSS"],
+    link: "https://rick-morty-characters-one.vercel.app/",
+    github: "https://github.com/raulboni/rick-morty-characters",
+  },
+  {
+    title: "Portfolio",
+    description:
+      "Website that shows projects and info to potential empoyers and includes a simple game to make it more dynamic.",
+    img: "/projects/rick-morty-characters.png",
+    techs: ["React.js", "Next.js", "API-REST", "Vercel", "Tailwind CSS"],
+    link: "https://rick-morty-characters-one.vercel.app/",
+    github: "https://github.com/raulboni/portfolio",
   },
 ];
